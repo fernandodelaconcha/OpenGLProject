4c01e9c39a848af33a38f5e47e517b23198f9bcf
Fernando de la Concha <fernandodelaconcha@gmail.com>
2024-05-08 09:40:20 +0200

    Add project files.

---
+++ TestOpenGL/TestOpenGL.cpp
@@ -0,0 +1,193 @@
+﻿// TestOpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "TestOpenGL.h"
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <string>
+
+static bool initializeGLFW()
+{
+	return glfwInit();
+}
+
+static bool initializeGLEW()
+{
+	return glewInit() == GLEW_OK;
+}
+
+void setWindowHints()
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+}
+
+void frameBufferResizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void handleKeyInput(GLFWwindow* window, int key, int status, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+	else {
+		std::cout << "key: " << key << "; status: " << status << "; action: " << action <<"; mods: " << mods << std::endl;
+	}
+}
+
+bool loadShaders(GLuint &program)
+{
+	bool loadSuccess = true;
+	char infoLog[512];
+	GLint success;
+
+	std::string temp = "";
+	std::string src = "";
+
+	std::ifstream in_file;
+
+	// vertex
+	in_file.open("vertex_core.glsl");
+
+	if (in_file.is_open()) {
+		while (std::getline(in_file, temp)) {
+			src += temp + "\n";
+		}
+	}
+	else {
+		std::cerr << "ERROR::LOADSHADERS::COULD_NOT_OPEN_VERTEX_FILE" << "\n";
+		loadSuccess = false;
+	}
+	in_file.close();
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	const GLchar* vertSrc = src.c_str();
+	glShaderSource(vertexShader, 1, &vertSrc, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::LOADSHADERS::COULD_NOT_COMPILE_VERTEX_SHADER" << "\n";
+		std::cout << infoLog << "\n";
+		loadSuccess = false;
+	}
+
+	temp = "";
+	src = "";
+
+	//fragment
+	in_file.open("fragment_core.glsl");
+
+	if (in_file.is_open()) {
+		while (std::getline(in_file, temp)) {
+			src += temp + "\n";
+		}
+	}
+	else {
+		std::cerr << "ERROR::LOADSHADERS::COULD_NOT_OPEN_FRAGMENT_FILE" << "\n";
+		loadSuccess = false;
+	}
+	in_file.close();
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const GLchar* fragSrc = src.c_str();
+	glShaderSource(fragmentShader, 1, &fragSrc, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::LOADSHADERS::COULD_NOT_COMPILE_FRAGMENT_SHADER" << "\n";
+		std::cout << infoLog << "\n";
+		loadSuccess = false;
+	}
+
+	temp = "";
+	src = "";
+
+
+	program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cerr << "ERROR::LOADSHADERS::COULD_NOT_LINK_PROGRAM" << "\n";
+		std::cout << infoLog << "\n";
+		loadSuccess = false;
+	}
+
+	glUseProgram(0);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return loadSuccess;
+}
+
+int main()
+{
+	if (!initializeGLFW()) {
+		std::cerr << "glfw initialization failed." << std::endl;
+		return -1;
+	}
+
+	setWindowHints();
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Super videogame", nullptr, nullptr);
+
+	glfwSetFramebufferSizeCallback(window, frameBufferResizeCallback);
+
+	if (window == nullptr) {
+		std::cerr << "GLFW window creation failed" << std::endl;
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, handleKeyInput);
+
+	glewExperimental = GL_TRUE;
+	if (!initializeGLEW()) {
+		std::cerr << "Glew initialization failed" << std::endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+
+	GLuint core_program;
+	if (!loadShaders(core_program)) {
+		glfwTerminate();
+	}
+
+	glClearColor(0.3f, 0.3f, 0.4f, 1.0f);
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	glDeleteProgram(core_program);
+
+	return 0;
+}
