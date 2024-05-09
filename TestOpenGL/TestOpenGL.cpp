4a074925d47c571cf5045f31bf3ff3b32aeb1546
Fernando de la Concha <fernandodelaconcha@gmail.com>
2024-05-09 10:18:00 +0200

    draw simple triangle

--- TestOpenGL/TestOpenGL.cpp
+++ TestOpenGL/TestOpenGL.cpp
@@ -7,6 +7,35 @@
 #include <GLFW/glfw3.h>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <glm/common.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 color;
+	glm::vec2 texcoord;
+};
+
+Vertex vertices[] =
+{
+	glm::vec3(0.0f, 0.5f, 0.f),glm::vec3(1.0f, 0.f, 0.f), glm::vec2(0.f, 1.f),
+	glm::vec3(-0.5f, -0.5f, 0.f),glm::vec3(0.f, 1.f, 0.f), glm::vec2(0.f, 0.f),
+	glm::vec3(0.5f, -0.5f, 0.f),glm::vec3(0.f, 0.f, 1.f), glm::vec2(1.f, 0.f)
+};
+
+unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
+
+GLuint indices[] =
+{
+	0,1,2
+};
+
+unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
 static bool initializeGLFW()
 {
@@ -185,12 +214,41 @@ int main()
 		glfwTerminate();
 	}
 
+	GLuint VAO;
+	glCreateVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+	glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0);
+
 	glClearColor(0.3f, 0.3f, 0.4f, 1.0f);
 	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(core_program);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 	}
